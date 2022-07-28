5ed4e449679321757ba75eafff176ef707006fa0
yoo <mi21a001@oecu.jp>
2022-07-28 23:53:45 +0900

    frontend  frameworkのalephを試してみた

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
-import { serve } from "./deps.ts";
+// import { serve } from "./deps.ts";
 
-const PORT = 1993;
-const s = serve(`0.0.0.0:${PORT}`);
-const body = new TextEncoder().encode("Hello World\n");
+// const PORT = 1993;
+// const s = serve(`0.0.0.0:${PORT}`);
+// const body = new TextEncoder().encode("tyin tyin Hello World\n");
 
-console.log(`Server started on port ${PORT}`);
-for await (const req of s) {
-  req.respond({ body });
-}
+// console.log(`Server started on port ${PORT}`);
+// for await (const req of s) {
+//   req.respond({ body });
+// }
+
+
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+serve((_req) => {
+  return new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+});
